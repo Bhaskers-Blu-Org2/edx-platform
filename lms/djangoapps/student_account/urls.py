479a02d11bc1f14f896858d8d85dada98a777b20
btelnes <btelnes@microsoft.com>
2018-12-27 17:03:57 -0800

    Add cookie banner to comprehensive theme for Hawthorn (#474)

--- lms/djangoapps/student_account/urls.py
+++ lms/djangoapps/student_account/urls.py
@@ -6,6 +6,7 @@ from student_account import views
 urlpatterns = [
     url(r'^finish_auth$', views.finish_auth, name='finish_auth'),
     url(r'^settings$', views.account_settings, name='account_settings'),
+    url(r'^get_cookies$', views.cookies_api, name='cookies_api'),
 ]
 
 if settings.FEATURES.get('ENABLE_COMBINED_LOGIN_REGISTRATION'):
